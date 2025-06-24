37fc95aeb2f27f36867474d8cd004a60f2cc794c
horoyoii <34915108+horoyoii@users.noreply.github.com>
2025-06-24 18:27:14 +0900

    Add workflow editor UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import WorkflowEditor from './WorkflowEditor'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WorkflowEditor />
   </React.StrictMode>
 )
